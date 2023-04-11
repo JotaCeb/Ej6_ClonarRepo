03168acec971d0c04b46ac6fd3823787c940039e
Cebrián <javiceb@hotmail.com>
2023-04-11 20:32:56 +0200

    Add files via upload

---
+++ src/main/java/Ejercicio6_ClonarRepo/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Ejercicio6_ClonarRepo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
